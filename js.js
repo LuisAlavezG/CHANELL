82a3458683784372f888d77f6f4d31b6e0e8b9ea
Luis Arturo <luisarturogarciaalavez@gmail.com>
2024-02-22 20:28:23 -0600

    Segunda forma  de  encriptar

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 
-
+/*
 function Encriptacion1(){    
     let PrimerTexto = document.getElementById("PrimerTexto").value;
     let key = 3;   
-
-
     let Prueba = (Encriptacion(PrimerTexto, key));
     //console.log(Prueba);
     document.getElementById("PrimerTexto").value = Prueba;
+   /* document.getElementById("CajaTexto2").value = Prueba;
+    document.getElementById("PrimerTexto").value = "";
 }
 
 
@@ -16,6 +16,7 @@ function Encriptacion( text,key)
     let codes = [];
     for(let i = 0 ; i < text.length ; i++){
         codes.push((text.charCodeAt(i) + key));
+        console.log(codes[i]);
     }
     let texto = String.fromCharCode(...codes); 
     //console.log(texto);
@@ -27,9 +28,7 @@ function Encriptacion( text,key)
 
 function Desencriptacion1(){    
     let PrimerTexto = document.getElementById("PrimerTexto").value;
-    let key = 3;   
-
-    
+    let key = 3;      
     let Prueba = (Desencriptacion(PrimerTexto, key));
     //console.log(Prueba);
     document.getElementById("PrimerTexto").value = Prueba;
@@ -46,9 +45,73 @@ function Desencriptacion( text,key)
     //console.log(texto);
     return texto;
 }
+*/
+
+let texto1 = document.querySelector("#PrimerTexto");
+let texto2 = document.querySelector("#CajaTexto2")
 
 
+const matriz_Codigo =[
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+
+];
 
 
+function Encriptacion1(){
+
+    const texto = Encriptacion(texto1.value);
+    console.log(texto);
+    texto2.value = texto;
+    texto1.value = " ";
+
+}
 
 
+function Encriptacion(Frace){
+
+    for( let i = 0; i <  matriz_Codigo.length ; i++){
+
+        if(Frace.includes(matriz_Codigo[i][0])){
+            
+            Frace = Frace.replaceAll(
+                matriz_Codigo[i][0],
+                matriz_Codigo[i][1]
+            )
+        }
+
+        
+    }
+    return Frace;
+}
+
+
+function Desencriptacion1(){
+
+    const texto = Desencriptacion(texto1.value);
+    console.log(texto);
+    texto2.value = texto;
+    texto1.value = " ";
+}
+
+
+function Desencriptacion(Frace){
+
+    for( let i = 0; i <  matriz_Codigo.length ; i++){
+
+        if(Frace.includes(matriz_Codigo[i][1])){
+            
+            Frace = Frace.replaceAll(
+                matriz_Codigo[i][1],
+                matriz_Codigo[i][0]
+                
+            )
+        }
+
+        
+    }
+    return Frace;
+}
